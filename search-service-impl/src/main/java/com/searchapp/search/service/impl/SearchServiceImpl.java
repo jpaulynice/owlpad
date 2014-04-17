9fd570e3c8418cc508aebf36a7caaefd4cd62be1
julesbond007 <jay.paulynice@gmail.com>
2014-04-16 21:29:35 -0400

    changes

--- search-service-impl/src/main/java/com/searchapp/search/service/impl/SearchServiceImpl.java
+++ search-service-impl/src/main/java/com/searchapp/search/service/impl/SearchServiceImpl.java
@@ -26,32 +26,35 @@ import org.apache.lucene.util.Version;
  * {@link SearchService} implementation.
  * 
  * @author Jay Paulynice
- *
+ * 
  */
 public class SearchServiceImpl implements SearchService {
 
 	/*
 	 * (non-Javadoc)
-	 * @see com.searchapp.search.service.SearchService#search(com.searchapp.search.domain.SearchRequest)
+	 * 
+	 * @see
+	 * com.searchapp.search.service.SearchService#search(com.searchapp.search
+	 * .domain.SearchRequest)
 	 */
 	@Override
-	public SearchResponse search(SearchRequest searchRequest) throws Exception {	
-		
-		//will need to specify index directory for searching...this is just an example
-		//since i have it already.
-		File indexDir = new File("/Users/julespaulynice/Documents/search/index");
-    	Directory directory = FSDirectory.open(indexDir);
+	public SearchResponse search(SearchRequest searchRequest) throws Exception {
 
-        String query = searchRequest.getKeyWord(); 
-        int hits = searchRequest.getMaxHits();
-        
-        return searchIndex(directory, query, hits);
-		
+		// will need to specify index directory for searching...this is just an
+		// example
+		// since i have it already.
+		File indexDir = new File("/Users/julespaulynice/Documents/search/index");
+		Directory directory = FSDirectory.open(indexDir);
+
+		String query = searchRequest.getKeyWord();
+		int hits = searchRequest.getMaxHits();
+
+		return searchIndex(directory, query, hits);
+
 	}
-	
+
 	/**
-	 * Search the index for our query string and return
-	 * only the maxHits count.
+	 * Search the index for our query string and return only the maxHits count.
 	 * 
 	 * @param indexDir
 	 * @param queryStr
@@ -59,35 +62,36 @@ public class SearchServiceImpl implements SearchService {
 	 * @return
 	 * @throws Exception
 	 */
-	private SearchResponse searchIndex(Directory indexDir, String queryStr, int maxHits) 
-            throws Exception {
-		
+	private SearchResponse searchIndex(Directory indexDir, String queryStr,
+			int maxHits) throws Exception {
+
 		SearchResponse response = new SearchResponse();
 		List<Document> results = new ArrayList<Document>();
-        
-        IndexReader indexReader = DirectoryReader.open(indexDir);
 
-        IndexSearcher searcher = new IndexSearcher(indexReader);
-        
-		QueryBuilder queryBuilder = new QueryBuilder(new StandardAnalyzer(Version.LUCENE_47));
-        Query query = queryBuilder.createPhraseQuery("contents",queryStr);
-        
-        TopDocs topDocs = searcher.search(query, maxHits);
-        
-        ScoreDoc[] hits = topDocs.scoreDocs;
-        
-        for (int i = 0; i < hits.length; i++) {
-            int docId = hits[i].doc;
-            org.apache.lucene.document.Document doc = searcher.doc(docId);
-            
-            Document d = new Document();
-            d.setDocId(docId);
-            d.setDocTitle(doc.get("filename"));
-            results.add(d);
-        }
-        response.setDocuments(results);
-                
-        return response;
-    }
+		IndexReader indexReader = DirectoryReader.open(indexDir);
+
+		IndexSearcher searcher = new IndexSearcher(indexReader);
+
+		QueryBuilder queryBuilder = new QueryBuilder(new StandardAnalyzer(
+				Version.LUCENE_47));
+		Query query = queryBuilder.createPhraseQuery("contents", queryStr);
+
+		TopDocs topDocs = searcher.search(query, maxHits);
+
+		ScoreDoc[] hits = topDocs.scoreDocs;
+
+		for (int i = 0; i < hits.length; i++) {
+			int docId = hits[i].doc;
+			org.apache.lucene.document.Document doc = searcher.doc(docId);
+
+			Document d = new Document();
+			d.setDocId(docId);
+			d.setDocTitle(doc.get("filename"));
+			results.add(d);
+		}
+		response.setDocuments(results);
+
+		return response;
+	}
 
 }
