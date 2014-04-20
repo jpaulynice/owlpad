997f2812676b4905ccbfdae8f30e23581caee45e
julesbond007 <jay.paulynice@gmail.com>
2014-04-20 12:16:07 -0400

    changes

--- search-app/src/main/webapp/resources/js/app/mediator.js
+++ search-app/src/main/webapp/resources/js/app/mediator.js
@@ -5,8 +5,20 @@
  */
 
 SearchAppMediator = {
-		initialize: function(){
+		initialize: function(options){
 			this.entities = new ajaxEntities();
-		}
+		},
 		
+		search: function(data){
+			var defer = $.Deferred();
+			var searchCall = this.entities.search(data);
+			
+			$.when(searchCall).done(function(jsonRes){
+				defer.resolve();
+				//fire event to say done
+			}).fail(function(){
+				defer.reject();
+			});
+			defer.promise();
+		}
 };
