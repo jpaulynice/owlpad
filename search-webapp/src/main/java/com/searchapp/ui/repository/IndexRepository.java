62162afb1b40b868fd3b0dbcf3f255b5d5d06279
julesbond007 <jay.paulynice@gmail.com>
2014-05-01 21:58:26 -0400

    changes

--- search-webapp/src/main/java/com/searchapp/ui/repository/IndexRepository.java
+++ search-webapp/src/main/java/com/searchapp/ui/repository/IndexRepository.java
@@ -1,5 +1,34 @@
 package com.searchapp.ui.repository;
 
-public class IndexRepository {
+import org.springframework.beans.factory.annotation.Autowired;
 
+import com.searchapp.index.domain.IndexRequest;
+import com.searchapp.index.domain.IndexResponse;
+import com.searchapp.index.service.IndexService;
+
+public class IndexRepository {
+	private IndexService indexService;
+	
+	@Autowired
+	public IndexRepository(IndexService indexService){
+		this.setIndexService(indexService);
+	}
+	
+	public IndexResponse index(IndexRequest indexRequest) {
+		return null;
+	}
+
+	/**
+	 * @return the indexService
+	 */
+	public IndexService getIndexService() {
+		return indexService;
+	}
+
+	/**
+	 * @param indexService the indexService to set
+	 */
+	public void setIndexService(IndexService indexService) {
+		this.indexService = indexService;
+	}
 }
--- search-webapp/src/main/java/com/searchapp/ui/web/IndexController.java
+++ search-webapp/src/main/java/com/searchapp/ui/web/IndexController.java
@@ -1,5 +1,33 @@
 package com.searchapp.ui.web;
 
-public class IndexController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.searchapp.index.domain.IndexRequest;
+import com.searchapp.index.domain.IndexResponse;
+import com.searchapp.ui.repository.IndexRepository;
+
+@Controller
+@RequestMapping(value = "/index")
+public class IndexController {
+	private IndexRepository indexRepository;
+
+	@Autowired
+	public IndexController(IndexRepository indexRepository){
+		this.indexRepository = indexRepository;
+	}
+	/**
+	 * Index end point
+	 * 
+	 * @param indexRequest
+	 * @return
+	 */
+	@RequestMapping(method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
+	public @ResponseBody IndexResponse search(@RequestBody IndexRequest indexRequest){
+		return indexRepository.index(indexRequest);
+	}
 }
