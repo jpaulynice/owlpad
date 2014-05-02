55c556afe3461ba836f77f0d9d2463bbd6da43c5
julesbond007 <jay.paulynice@gmail.com>
2014-05-02 16:49:09 -0400

    fix bug when 0 results returned

--- search-webapp/src/main/webapp/resources/js/app/controllers/controller.js
+++ search-webapp/src/main/webapp/resources/js/app/controllers/controller.js
@@ -39,23 +39,25 @@ define([ 'jquery', 'underscore', 'backbone', 'marionette',
 		 * After results come back, we want to show the table data
 		 */
 		showGridResuls : function(data) {			
-			var headers = [];
-			for(var key in data.documents[0].fields){
-				headers.push(data.documents[0].fields[key].name);
-			}
-			
-			var coll = new Backbone.Collection();
-			
-			for(var i in data.documents){
-				var fields = data.documents[i].fields;
-				coll.add(new Backbone.Model({fields:fields}));
-			}
+			if(data.documents && data.documents.length>0){
+				var headers = [];
+				for(var key in data.documents[0].fields){
+					headers.push(data.documents[0].fields[key].name);
+				}
+				
+				var coll = new Backbone.Collection();
+				
+				for(var i in data.documents){
+					var fields = data.documents[i].fields;
+					coll.add(new Backbone.Model({fields:fields}));
+				}
 
-			var gridView = new Grid({
-				collection : coll,
-				headers : headers
-			});
-			this.content.show(gridView);
+				var gridView = new Grid({
+					collection : coll,
+					headers : headers
+				});
+				this.content.show(gridView);
+			}
 		}
 
 	});
