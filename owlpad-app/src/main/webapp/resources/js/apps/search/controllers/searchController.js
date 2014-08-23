e2e76e9d7485fea877e5ea17f344594746b8c8f1
julesbond007 <jay.paulynice@gmail.com>
2014-08-23 11:41:23 -0400

    clean up

--- owlpad-app/src/main/webapp/resources/js/apps/search/controllers/searchController.js
+++ owlpad-app/src/main/webapp/resources/js/apps/search/controllers/searchController.js
@@ -1,36 +1,36 @@
 /**
  * Search controller to handle search
- * 
+ *
  */
-define([ 'jquery', 
-         'underscore', 
-         'backbone', 
-         'marionette',
-         'apps/search/entities/searchEntities'], 
-         function($, _,Backbone, Marionette, SearchEntities) {
+define(['jquery', 
+        'underscore', 
+        'backbone', 
+        'marionette', 
+        'apps/search/entities/searchEntities'], 
+        function($, _, Backbone, Marionette, SearchEntities) {
 
-	var SearchAppController = Backbone.Marionette.Controller.extend({
-		initialize : function(options) {
-			this.searchEntities = new SearchEntities();
-		},
+    var SearchAppController = Backbone.Marionette.Controller.extend({
+        initialize : function(options) {
+            this.searchEntities = new SearchEntities();
+        },
 
-		/**
-		 * Search end point to call search from the controller.
-		 */
-		search : function(data) {
-			var defer = $.Deferred();
-			var searchCall = this.searchEntities.search(data);
+        /**
+         * Search end point to call search from the controller.
+         */
+        search : function(data) {
+            var defer = $.Deferred();
+            var searchCall = this.searchEntities.search(data);
 
-			$.when(searchCall).done(function(jsonRes) {
-				defer.resolve(jsonRes);
-			}).fail(function() {
-				defer.reject();
-			});
-			
-			return defer.promise();
-		},
+            $.when(searchCall).done(function(jsonRes) {
+                defer.resolve(jsonRes);
+            }).fail(function() {
+                defer.reject();
+            });
 
-        getDocById: function(docId) {
+            return defer.promise();
+        },
+
+        getDocById : function(docId) {
             var defer = $.Deferred();
             var call = this.searchEntities.getDocById(docId);
 
@@ -42,8 +42,7 @@ define([ 'jquery',
 
             return defer.promise();
         },
+    });
 
-	});
-
-	return SearchAppController;
-});
+    return SearchAppController;
+}); 
