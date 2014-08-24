6341c42c19d7af258336f9c03a4bc668d1fca296
julesbond007 <jay.paulynice@gmail.com>
2014-08-23 23:17:58 -0400

    work on paging search results.

--- owlpad-app/src/main/webapp/resources/js/build.js
+++ owlpad-app/src/main/webapp/resources/js/build.js
@@ -1,22 +1,17 @@
-({
-  "baseUrl": "./",
-  "mainConfigFile": "main.js",
-  "dir":"dist",
-  "optimize": "uglify2", 
-  "removeCombined": true,
-  "findNestedDependencies": true,
-  modules: [
-    {
-        name: 'main'
-    },
-    {
-        name: 'search',
-        exclude: ['main']
-    },
-    
-    {
-        name: 'index',
-        exclude: ['main']
-    }
-]
+( {
+    "baseUrl" : "./",
+    "mainConfigFile" : "main.js",
+    "dir" : "dist",
+    "optimize" : "uglify2",
+    "removeCombined" : true,
+    "findNestedDependencies" : true,
+    modules : [{
+        name : 'main'
+    }, {
+        name : 'search',
+        exclude : ['main']
+    }, {
+        name : 'index',
+        exclude : ['main']
+    }]
 })
--- owlpad-app/src/main/webapp/resources/js/main.js
+++ owlpad-app/src/main/webapp/resources/js/main.js
@@ -2,52 +2,52 @@
  * Application configuration with shortcut alias
  */
 require.config({
-	paths : {
-		hbs : 'vendor/hbs',
-		jquery : 'vendor/jquery',
-		json2 : 'vendor/json2',
-		underscore : 'vendor/underscore',
-		handlebars : 'vendor/handlebars',
-		backbone : 'vendor/backbone',
-		marionette : 'vendor/backbone.marionette.min',
-		i18nprecompile : 'vendor/i18nprecompile',
-		bootstrap:'vendor/bootstrap.min',
-		syntaxHighlighter: 'vendor/syntaxHighlighter/shCore.min',
-		shBrush: 'vendor/syntaxHighlighter/shBrushJava'
-	},
+    paths : {
+        hbs : 'vendor/hbs',
+        jquery : 'vendor/jquery',
+        json2 : 'vendor/json2',
+        underscore : 'vendor/underscore',
+        handlebars : 'vendor/handlebars',
+        backbone : 'vendor/backbone',
+        marionette : 'vendor/backbone.marionette.min',
+        i18nprecompile : 'vendor/i18nprecompile',
+        bootstrap : 'vendor/bootstrap.min',
+        syntaxHighlighter : 'vendor/syntaxHighlighter/shCore.min',
+        shBrush : 'vendor/syntaxHighlighter/shBrushJava'
+    },
 
-	shim : {
-		jquery : {
-			exports : '$'
-		},
-		json2 : {
-			exports : 'JSON'
-		},
-		underscore : {
-			exports : '_'
-		},
-		handlebars : {
-			exports : 'Handlebars'
-		},
-		backbone : {
-			deps : [ 'jquery', 'json2', 'underscore' ],
-			exports : 'Backbone'
-		},
-		marionette : {
-			deps : [ 'jquery', 'underscore', 'backbone' ],
-			exports : 'Marionette'
-		},
-		bootstrap:{
-			deps:['jquery'],
-			exports:'Bootstrap'
-		},
-		syntaxHighlighter:{
-		    deps:['jquery'],
-		    exports:'SyntaxHighlighter'
-		},
-		shBrush:{
-            deps:['syntaxHighlighter'],
-            exports:'shBrush'
+    shim : {
+        jquery : {
+            exports : '$'
+        },
+        json2 : {
+            exports : 'JSON'
+        },
+        underscore : {
+            exports : '_'
+        },
+        handlebars : {
+            exports : 'Handlebars'
+        },
+        backbone : {
+            deps : ['jquery', 'json2', 'underscore'],
+            exports : 'Backbone'
+        },
+        marionette : {
+            deps : ['jquery', 'underscore', 'backbone'],
+            exports : 'Marionette'
+        },
+        bootstrap : {
+            deps : ['jquery'],
+            exports : 'Bootstrap'
+        },
+        syntaxHighlighter : {
+            deps : ['jquery'],
+            exports : 'SyntaxHighlighter'
+        },
+        shBrush : {
+            deps : ['syntaxHighlighter'],
+            exports : 'shBrush'
         }
-	}
-});
+    }
+}); 
