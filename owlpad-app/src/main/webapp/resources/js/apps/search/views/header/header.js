bd55e8f97d4a40ac258f9e312ed71877bb2b8c38
julesbond007 <jay.paulynice@gmail.com>
2014-08-23 23:28:40 -0400

    work on paging search results.

--- owlpad-app/src/main/webapp/resources/js/apps/search/views/header/header.js
+++ owlpad-app/src/main/webapp/resources/js/apps/search/views/header/header.js
@@ -1,42 +1,44 @@
-define([ 'jquery', 
-         'underscore', 
-         'backbone', 
-         'marionette',
-         'bootstrap',
-         'hbs!apps/search/templates/header/header' ],
-         function($, _, Backbone, Marionette,Bootstrap, headerTemplate) {
-	
-	var HeaderView = Backbone.Marionette.ItemView.extend({
-		template:headerTemplate,
-		className:"container-fluid",
-		ui:{
-			searchText:".search-input"
-		},
-		
-		events:{
-			"keypress .search-input": "handleEnter",
-			"click .fa-search": "handleSearch"
-		},
-		
-		initialize: function(options){
-		
-		},
-		
-		handleEnter: function(e){
-			if ( e.which === 13 ) { 
-		        this.handleSearch();
-			}
-		},
-		
-		handleSearch:function(){
-			var keyWord = this.ui.searchText.val();
-			if(keyWord){
-				var data = {keyWord:keyWord,maxHits:10};
-				this.trigger('app:view:search',data);
-			}
-		}
-	});
+define(['jquery', 
+        'underscore', 
+        'backbone', 
+        'marionette', 
+        'bootstrap', 
+        'hbs!apps/search/templates/header/header'], 
+        function($, _, Backbone, Marionette, Bootstrap, headerTemplate) {
 
+    var HeaderView = Backbone.Marionette.ItemView.extend({
+        template : headerTemplate,
+        className : "container-fluid",
+        ui : {
+            searchText : ".search-input"
+        },
 
-	return HeaderView;
-});
+        events : {
+            "keypress .search-input" : "handleEnter",
+            "click .fa-search" : "handleSearch"
+        },
+
+        initialize : function(options) {
+
+        },
+
+        handleEnter : function(e) {
+            if (e.which === 13) {
+                this.handleSearch();
+            }
+        },
+
+        handleSearch : function() {
+            var keyWord = this.ui.searchText.val();
+            if (keyWord) {
+                var data = {
+                    keyWord : keyWord,
+                    maxHits : 10
+                };
+                this.trigger('app:view:search', data);
+            }
+        }
+    });
+
+    return HeaderView;
+}); 
