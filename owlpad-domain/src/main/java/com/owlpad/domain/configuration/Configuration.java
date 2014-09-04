c435bc86f09ac7a207087998bda27ee4d1886fe9
julesbond007 <jay.paulynice@gmail.com>
2014-09-04 18:37:25 -0400

    fix tests

--- owlpad-domain/src/main/java/com/owlpad/domain/configuration/Configuration.java
+++ owlpad-domain/src/main/java/com/owlpad/domain/configuration/Configuration.java
@@ -1,7 +1,6 @@
 package com.owlpad.domain.configuration;
 
 import javax.persistence.CascadeType;
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
