e761618e0dc532dd7fdccf1f3c59f072f84ffa35
julesbond007 <jay.paulynice@gmail.com>
2014-09-05 13:04:25 -0400

    add sql code for application configuration

--- owlpad-service-impl/src/main/resources/META-INF/db/db.sql
+++ owlpad-service-impl/src/main/resources/META-INF/db/db.sql
@@ -1,8 +1,7 @@
 drop schema if exists owlpaddb;
 create schema owlpaddb;
-user owlpaddb;
+use owlpaddb;
 
-//layout table
 create table `layout` (
   id int(11) NOT NULL AUTO_INCREMENT,
   header_region varchar(45) DEFAULT NULL,
@@ -12,17 +11,15 @@ create table `layout` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
-//configuration table
 create table `configuration` (
   id int(11) NOT NULL AUTO_INCREMENT,
-  name varchar(45) DEFAUL NULL,
+  name varchar(45) DEFAULT NULL,
   layout_id int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_conf` (`layout_id`),
   CONSTRAINT `fk_conf` FOREIGN KEY (`layout_id`) REFERENCES `layout` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
-//initialize with default values
 insert into layout (id,header_region,left_region,right_region,footer_region)
 values (1,'.navbar-default', '.gridRegion', '.previewRegion', '.footer');
 
